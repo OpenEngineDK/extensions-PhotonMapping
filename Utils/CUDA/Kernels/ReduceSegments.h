ce6889c2d0dff5c9244fb7f2315cd388c55dcd93
asgerhoedt <asgerhoedt@gmail.com>
2011-03-17 14:24:30 +0000

    Removed useless kernels from ReduceSegments.h and added a new segmented reduce kernel that appears to be working \o?

--- Utils/CUDA/Kernels/ReduceSegments.h
+++ Utils/CUDA/Kernels/ReduceSegments.h
@@ -23,58 +23,6 @@ namespace Kernels {
 #include <Utils/CUDA/SharedMemory.h>
 #include <Utils/CUDA/Utils.h>
 
-/**
- * Optimizations yield ~50%
- *
- * Remove unnecessary syncthreads.
- * Use register to avoid a shared memory lookup.
- * Unroll final reduction into the output step.
- * Explicitly kill threads that will no longer do anything useful.
- *
- * PENDING
- * Do a complete unroll, since the freaking compiler can't!!
- * use float3 in shared mem and register variable
- */
-__global__ void 
-__launch_bounds__(Segments::SEGMENT_SIZE) 
-    ReduceSegments(int2 *primInfo,
-                   float4 *aabbMin, float4 *aabbMax,
-                   float4 *minResult, float4 *maxResult){
-        
-    const int segmentID = blockIdx.x;
-
-    if (segmentID < d_segments){
-        float4* sharedMin = SharedMemory<float4>();
-        float4* sharedMax = sharedMin + Segments::SEGMENT_SIZE;
-            
-        const int2 primitiveInfo = primInfo[segmentID];
-
-        float4 localMin = sharedMin[threadIdx.x] = threadIdx.x < primitiveInfo.y ? aabbMin[threadIdx.x + primitiveInfo.x] : make_float4(fInfinity);
-        float4 localMax = sharedMax[threadIdx.x] = threadIdx.x < primitiveInfo.y ? aabbMax[threadIdx.x + primitiveInfo.x] : make_float4(-1.0f * fInfinity);
-        __syncthreads();
-            
-        // Reduce!
-        for (int offset = Segments::SEGMENT_SIZE/2; offset > warpSize; offset /= 2){
-            if (threadIdx.x < offset){
-                sharedMin[threadIdx.x] = localMin = min(localMin, sharedMin[threadIdx.x + offset]);
-                sharedMax[threadIdx.x] = localMax = max(localMax, sharedMax[threadIdx.x + offset]);
-            }else
-                return;
-            __syncthreads();
-        }
-            
-        for (int offset = warpSize; offset > 1; offset /= 2){
-            sharedMin[threadIdx.x] = localMin = min(localMin, sharedMin[threadIdx.x + offset]);
-            sharedMax[threadIdx.x] = localMax = max(localMax, sharedMax[threadIdx.x + offset]);
-        }
-
-        if (threadIdx.x == 0){
-            minResult[segmentID] = min(localMin, sharedMin[1]);
-            maxResult[segmentID] = max(localMax, sharedMax[1]);
-        }
-    }
-}
-
 __device__ void fminf3(volatile float *v, volatile float *u, volatile float *ret){
     ret[0] = min(v[0], u[0]);
     ret[1] = min(v[1], u[1]);
@@ -100,6 +48,8 @@ __device__ void reduceFmaxf3(volatile float *v, volatile float *u, volatile floa
     ret[2] = v[2] = max(v[2], u[2]);
 }
 
+
+// @TODO write owner into the .w component? .o?
 
 __global__ void
 __launch_bounds__(Segments::SEGMENT_SIZE) 
@@ -375,45 +325,6 @@ __launch_bounds__(Segments::SEGMENT_SIZE)
     }
 }    
 
-__global__ void 
-__launch_bounds__(Segments::SEGMENT_SIZE) 
-    GlobalReduceSegments(int2 *primInfo,
-                         float4 *aabbMin, float4 *aabbMax,
-                         float4 *minResult, float4 *maxResult){
-        
-    const int segmentID = blockIdx.x;
-
-    if (segmentID < d_segments){
-        const int2 primitiveInfo = primInfo[segmentID];
-
-        // Reduce!
-        for (int offset = Segments::SEGMENT_SIZE/2; offset > warpSize; offset /= 2){
-            if (threadIdx.x < offset){
-                aabbMin[threadIdx.x + primitiveInfo.x] = min(aabbMin[threadIdx.x + primitiveInfo.x], 
-                                                             aabbMin[threadIdx.x + offset + primitiveInfo.x]);
-                aabbMax[threadIdx.x + primitiveInfo.x] = max(aabbMax[threadIdx.x + primitiveInfo.x], 
-                                                             aabbMax[threadIdx.x + offset + primitiveInfo.x]);
-            }else
-                return;
-            __syncthreads();
-        }
-            
-        for (int offset = warpSize; offset > 1; offset /= 2){
-            aabbMin[threadIdx.x + primitiveInfo.x] = min(aabbMin[threadIdx.x + primitiveInfo.x], 
-                                                         aabbMin[threadIdx.x + offset + primitiveInfo.x]);
-            aabbMax[threadIdx.x + primitiveInfo.x] = max(aabbMax[threadIdx.x + primitiveInfo.x], 
-                                                         aabbMax[threadIdx.x + offset + primitiveInfo.x]);
-        }
-
-        if (threadIdx.x == 0){
-            minResult[segmentID] = min(aabbMin[primitiveInfo.x], aabbMin[1 + primitiveInfo.x]);
-            maxResult[segmentID] = max(aabbMax[primitiveInfo.x], aabbMax[1 + primitiveInfo.x]);
-        }
-    }
-}
-
-// @OPT Use shared memory. For God's sake make it work.
-
 __global__ void AabbMemset(float4* aabbMin, float4* aabbMax){
     int id = blockDim.x * blockIdx.x + threadIdx.x;
 
@@ -456,6 +367,99 @@ __global__ void FinalSegmentedReduce(float4 *segmentAabbMin,
         int owner = segmentOwner[0] - d_activeNodeIndex;
         nodeAabbMin[owner] = min(nodeAabbMin[owner], segmentAabbMin[0]);
         nodeAabbMax[owner] = max(nodeAabbMax[owner], segmentAabbMax[0]);
+    }
+}
+
+#define MAX_SEGS_PR_KERNEL 4
+
+__global__ void
+__launch_bounds__(MAX_SEGS_PR_KERNEL) 
+    InitializeHat(float4 *resMin, float4 *resMax, int segments){
+
+    const int id = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (id < segments){
+        resMin[id] = make_float4(fInfinity);
+        resMax[id] = make_float4(-1.0f * fInfinity);
+    }
+}
+
+__global__ void
+__launch_bounds__(MAX_SEGS_PR_KERNEL) 
+    TestingHat(float4 *inMin, float4 *inMax, int* inOwner,
+               float4 *resMin, float4* resMax,
+               float4* tempMin, float4* tempMax, int* tempOwner,
+               int segments){
+
+    const int startIndex = blockIdx.x * MAX_SEGS_PR_KERNEL;    
+    int range = segments - startIndex;
+    range = range > MAX_SEGS_PR_KERNEL ? MAX_SEGS_PR_KERNEL : range;
+
+    int ownerStart = inOwner[startIndex];
+    int ownerRange = inOwner[startIndex + range-1] - ownerStart + 1;
+
+    __shared__ int owner[MAX_SEGS_PR_KERNEL];
+    volatile __shared__ float sMin[3*MAX_SEGS_PR_KERNEL];
+    volatile __shared__ float sMax[3*MAX_SEGS_PR_KERNEL];
+    if (threadIdx.x < range){
+        owner[threadIdx.x] = inOwner[threadIdx.x + startIndex] - ownerStart;
+        float3 global = make_float3(inMin[threadIdx.x + startIndex]);
+        sMin[3*threadIdx.x] = global.x; sMin[3*threadIdx.x+1] = global.y; sMin[3*threadIdx.x+2] = global.z;
+        global = make_float3(inMax[threadIdx.x + startIndex]);
+        sMax[3*threadIdx.x] = global.x; sMax[3*threadIdx.x+1] = global.y; sMax[3*threadIdx.x+2] = global.z;
+    }
+
+    volatile __shared__ float rMin[3*MAX_SEGS_PR_KERNEL];
+    volatile __shared__ float rMax[3*MAX_SEGS_PR_KERNEL];
+    if (threadIdx.x < ownerRange){
+        float3 res = make_float3(resMin[ownerStart + threadIdx.x]);
+        rMin[3*threadIdx.x] = res.x; rMin[3*threadIdx.x+1] = res.y; rMin[3*threadIdx.x+2] = res.z;
+        res = make_float3(resMax[ownerStart + threadIdx.x]);
+        rMax[3*threadIdx.x] = res.x; rMax[3*threadIdx.x+1] = res.y; rMax[3*threadIdx.x+2] = res.z;
+    }
+    __syncthreads();
+    
+    int index0 = threadIdx.x * 2;
+    int index1 = index0+1;
+    while(index1 < range){
+        int owner0 = owner[index0];
+        int owner1 = owner[index1];
+        
+        if (owner0 == owner1){
+            sMin[3*index0] = min(sMin[3*index0], sMin[3*index1]);
+            sMin[3*index0+1] = min(sMin[3*index0+1], sMin[3*index1+1]);
+            sMin[3*index0+2] = min(sMin[3*index0+2], sMin[3*index1+2]);
+            sMax[3*index0] = max(sMax[3*index0], sMax[3*index1]);
+            sMax[3*index0+1] = max(sMax[3*index0+1], sMax[3*index1+1]);
+            sMax[3*index0+2] = max(sMax[3*index0+2], sMax[3*index1+2]);
+        }else{
+            rMin[3*owner1] = min(rMin[3*owner1], sMin[3*index1]);
+            rMin[3*owner1+1] = min(rMin[3*owner1+1], sMin[3*index1+1]);
+            rMin[3*owner1+2] = min(rMin[3*owner1+2], sMin[3*index1+2]);
+            rMax[3*owner1] = max(rMax[3*owner1], sMax[3*index1]);
+            rMax[3*owner1+1] = max(rMax[3*owner1+1], sMax[3*index1+1]);
+            rMax[3*owner1+2] = max(rMax[3*owner1+2], sMax[3*index1+2]);
+        }
+        
+        index0 *= 2;
+        index1 *= 2;
+        __syncthreads();
+    }
+
+    // Dump results
+    if (threadIdx.x != 0 && threadIdx.x < ownerRange){
+        resMin[ownerStart + threadIdx.x] = make_float4(rMin[3*threadIdx.x], 
+                                                       rMin[3*threadIdx.x+1],
+                                                       rMin[3*threadIdx.x+2], 0);
+        resMax[ownerStart + threadIdx.x] = make_float4(rMax[3*threadIdx.x],
+                                                       rMax[3*threadIdx.x+1],
+                                                       rMax[3*threadIdx.x+2], 0);
+    }
+
+    if (threadIdx.x == 0){
+        tempMin[blockIdx.x] = make_float4(sMin[0], sMin[1], sMin[2], 0);
+        tempMax[blockIdx.x] = make_float4(sMax[0], sMax[1], sMax[2], 0);
+        tempOwner[blockIdx.x] = ownerStart;
     }
 }
 
